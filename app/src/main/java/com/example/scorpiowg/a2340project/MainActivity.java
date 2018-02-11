e13d7bfa2d71e528d9a6abdf9da2b68350ea096c
JingboWang1997 <wjbjimmy@gmail.com>
2018-02-11 14:48:41 -0500

    init login and registration xml and activities

--- app/src/main/java/com/example/scorpiowg/a2340project/MainActivity.java
+++ app/src/main/java/com/example/scorpiowg/a2340project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.scorpiowg.a2340project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.login);
+        Button registration = findViewById(R.id.registration);
+
+        final Intent loginPage = new Intent(this, LoginActivity.class);
+        final Intent registerPage = new Intent(this, RegisterActivity.class);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                startActivity(loginPage);
+
+            }
+        });
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                startActivity(registerPage);
+
+            }
+        });
+
     }
 }
