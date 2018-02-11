4a14af7e3df48cb8976c03c0f332118f14d32f22
JingboWang1997 <wjbjimmy@gmail.com>
2018-02-11 14:53:10 -0500

    init login and registration xml and activities

---
+++ app/src/main/java/com/example/scorpiowg/a2340project/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.scorpiowg.a2340project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by wangjingbo on 2/11/18.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+}
---
+++ app/src/main/java/com/example/scorpiowg/a2340project/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.scorpiowg.a2340project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wangjingbo on 2/11/18.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+    }
+}
