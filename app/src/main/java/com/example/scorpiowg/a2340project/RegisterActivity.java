9ca580d0cdc7d5e804e45673ee5e21138a90babf
Unknown <4321konrad@gmail.com>
2018-02-12 15:51:29 -0500

    update RegisterActivity

--- app/src/main/java/com/example/scorpiowg/a2340project/RegisterActivity.java
+++ app/src/main/java/com/example/scorpiowg/a2340project/RegisterActivity.java
@@ -1,7 +1,12 @@
 package com.example.scorpiowg.a2340project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by wangjingbo on 2/11/18.
@@ -13,5 +18,39 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
 
+        if (getIntent().getStringExtra("error") != null) {
+            TextView error = findViewById(R.id.error);
+            error.setVisibility(View.VISIBLE);
+        }
+
+        Button cancelRegister = findViewById(R.id.cancel);
+        Button register = findViewById(R.id.register);
+
+        final EditText userinput = findViewById(R.id.username);
+        final EditText passinput = findViewById(R.id.password);
+        final EditText confinput = findViewById(R.id.confirm);
+
+        final Intent mainPage = new Intent(this, MainActivity.class);
+        final Intent loginPage = new Intent(this, LoginActivity.class);
+        final Intent registerPage = new Intent(this, RegisterActivity.class);
+
+        cancelRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(mainPage);
+            }
+        });
+
+         register.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (passinput.getText().toString().equals(confinput.getText().toString())) {
+                        startActivity(loginPage);
+                    } else {
+                        Bundle b = new Bundle();
+                        b.putString("error", "true");
+                        registerPage.putExtras(b);
+                        startActivity(registerPage);
+                    }
+                }
+            });
     }
 }
