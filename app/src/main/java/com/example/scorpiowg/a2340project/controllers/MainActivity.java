6fa78cacb3666f1bf620704157ab050f6d28c6fe
JingboWang1997 <wjbjimmy@gmail.com>
2018-02-17 17:12:04 -0500

    registration complete

--- app/src/main/java/com/example/scorpiowg/a2340project/controllers/MainActivity.java
+++ app/src/main/java/com/example/scorpiowg/a2340project/controllers/MainActivity.java
@@ -1,11 +1,16 @@
-package com.example.scorpiowg.a2340project;
+package com.example.scorpiowg.a2340project.controllers;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
+import com.example.scorpiowg.a2340project.R;
+import com.example.scorpiowg.a2340project.model.Model;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("debug", Integer.toString(Model.getInstance().getDatabase().size()));
 
+        // two options:login and registration
         Button login = findViewById(R.id.login);
         Button registration = findViewById(R.id.registration);
 
+        // next actions after button clicked
         final Intent loginPage = new Intent(this, LoginActivity.class);
         final Intent registerPage = new Intent(this, RegisterActivity.class);
 
--- app/src/main/java/com/example/scorpiowg/a2340project/controllers/RegisterActivity.java
+++ app/src/main/java/com/example/scorpiowg/a2340project/controllers/RegisterActivity.java
@@ -1,12 +1,18 @@
-package com.example.scorpiowg.a2340project;
+package com.example.scorpiowg.a2340project.controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.scorpiowg.a2340project.R;
+import com.example.scorpiowg.a2340project.model.Model;
+
+import java.util.HashMap;
 
 /**
  * Created by wangjingbo on 2/11/18.
@@ -18,6 +24,10 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
 
+        //database hashmap
+        final HashMap<String, String> database = Model.getInstance().getDatabase();
+
+        // check if this page was loaded from a registration error
         String error = getIntent().getStringExtra("error");
         if (error != null) {
             TextView passwordError = findViewById(R.id.passwordCheck);
@@ -32,34 +42,46 @@ public class RegisterActivity extends AppCompatActivity {
             }
         }
 
+        // two buttons
         Button cancelRegister = findViewById(R.id.cancel);
         Button register = findViewById(R.id.register);
 
-        final EditText userinput = findViewById(R.id.username);
-        final EditText passinput = findViewById(R.id.password);
-        final EditText confinput = findViewById(R.id.confirm);
 
+        // init next actions
         final Intent mainPage = new Intent(this, MainActivity.class);
         final Intent loginPage = new Intent(this, LoginActivity.class);
         final Intent registerPage = new Intent(this, RegisterActivity.class);
 
+        // click actions
         cancelRegister.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(mainPage);
             }
         });
-
-         register.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    if (passinput.getText().toString().equals(confinput.getText().toString()) && !userinput.getText().toString().equals("user")) {
+                    // inputs
+                    EditText userinput = findViewById(R.id.username);
+                    EditText passinput = findViewById(R.id.password);
+                    EditText confinput = findViewById(R.id.confirm);
+                    String userId = userinput.getText().toString();
+                    String password = passinput.getText().toString();
+                    String confirm = confinput.getText().toString();
+
+                    // next action
+                    if (password.equals(confirm) && !database.containsKey(userId)) {
+                        database.put(userId, password);
+                        loginPage.putExtra("database",database);
+                        Log.d("debug", Integer.toString(database.size()));
                         startActivity(loginPage);
                     } else {
-                        if (!passinput.getText().toString().equals(confinput.getText().toString()) && userinput.getText().toString().equals("user")) {
+                        registerPage.putExtra("database", database);
+                        if (!password.equals(confirm) && database.containsKey(userId)) {
                             Bundle b = new Bundle();
                             b.putString("error", "all");
                             registerPage.putExtras(b);
                             startActivity(registerPage);
-                        } else if (!passinput.getText().toString().equals(confinput.getText().toString())) {
+                        } else if (!password.equals(confirm)) {
                             Bundle b = new Bundle();
                             b.putString("error", "password");
                             registerPage.putExtras(b);
