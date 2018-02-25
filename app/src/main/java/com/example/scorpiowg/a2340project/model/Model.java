1057ce245190655c64a9b8cc5412e0328daaf0c0
Ruinan <nancywng@sina.com>
2018-02-25 15:36:58 -0500

    Finished CSV reading

--- app/src/main/java/com/example/scorpiowg/a2340project/model/Model.java
+++ app/src/main/java/com/example/scorpiowg/a2340project/model/Model.java
@@ -5,6 +5,7 @@ import android.support.compat.BuildConfig;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class Model {
@@ -12,17 +13,25 @@ public class Model {
     private static final Model _instance = new Model();
     public static Model getInstance() { return _instance; }
 
-    /** holds the list of all courses */
+    /** holds the list of all users */
     private HashMap<String, User> database;
+
+    /** holds the list of all shelters */
+    private Map<Integer, String[]> shelterdb;
+
 
     /**
      * make a new model
      */
     private Model() {
         database = new HashMap<>();
+        shelterdb = new HashMap<>();
     }
 
     public HashMap getDatabase() {
         return database;
+    }
+    public void setShelterdb(Map<Integer, String[]> shelterInfo) {
+        shelterdb = shelterInfo;
     }
 }
